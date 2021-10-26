b2d3649518f5f7a4d3f4377357450f1f9c6ba1b8
gmaisuradze <gmaisuradze@europebet.com>
2021-10-26 15:20:37 +0400

    first commit

---
+++ src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/task.service';
+
+@Component({
+  selector: 'app-task-view',
+  templateUrl: './task-view.component.html',
+  styleUrls: ['./task-view.component.scss']
+})
+export class TaskViewComponent implements OnInit {
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit() {
+  }
+
+  createNewList() {
+    this.taskService.createList('Test').subscribe((response) => {
+      console.log(response);
+    })
+  }
+
+}
---
+++ src/app/task.service.ts
@@ -0,0 +1,14 @@
+import { WebRequestService } from './web-request.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor(private webRequestService: WebRequestService) { }
+
+  createList(title: string) {
+    return this.webRequestService.post('lists', { title });
+  }
+}
